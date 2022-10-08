880ead71a74d6dbdeb3a5b568e53a299c8e4e012
203420 <203420@ids.upchiapas.edu.mx>
2022-10-08 18:14:17 -0500

    Correcion proyecto

--- prueba.py
+++ prueba.py
@@ -2,37 +2,31 @@ def get_List1():
 	return ["a", 19, "qz"]
 
 def prueba():
-	l____ = ["5","6","7"]
-	data = ["aaa", "b", "Z"]
-	#Se espera aceptación
-	for a in l____:
-		print(a) 
-	#Se espera aceptación
-	for A in ["1","2","3"]:
-		print(A)
-	#Se espera aceptación
-	for Z in [1,"2"]:
-		print(Z)
-	#Se espera aceptación
-	for _ in range(5):
-		print("3")
-	#Se espera NO aceptación
-	for xFDa in range(-5, 7):
-		print("5")
-	#Se espera NO aceptación		
-	for 9n in data:
-		print(9n)
-	#Se espera aceptación
-	for g in get_List1():
-		print(g)
-	#Se espera NO aceptación
-	for *AB in range(4):
-		print(*AB)
-	#Se espera NO aceptación
-	for datos in range(-5,-1):
-		print(datos)
+    lisTa_datos1 = ["5" ,6, "7"]
+
+    for i in        lisTa_datos1:
+        print(i)
+
+    for _num  in   range   (  -5  ,  10  )  :
+        print(_num)
+
+    for DATA in [ 1, 2,5, 6]:
+        print(DATA)
+
+    for e77 in get_List1():
+        print(e77)
+
+    #Condicion invalida
+    for 9e in get_List1():
+        print(9e)
+
+    #Condicion invalida
+    for   * in lisTa_datos1:
+        print(*)
+
+    #Condicion invalida
+    for  a in ["a","b"]]:
+        print(a)
 
 if __name__=="__main__":
 	prueba()
-
-
